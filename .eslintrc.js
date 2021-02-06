72308ecea1d30c552504f051cb37d97839b55963
kairov2605 <kairov2605@gmail.com>
2021-02-06 23:37:39 +0600

    начальная сборка , елемент юай, логин форма

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
+  // linebreak-style: ["error", "windows"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-page">
+    <div class="login-from-wrap">
+      <div class="login-form-image">
+        <img src="@/assets/background.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped>
+  .login-page{
+    background-color: rgb(210, 224, 217);
+    height: 100vh;
+    min-height: 780px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-from-wrap{
+    width: 100%;
+    max-width: 1000px;
+    background-color: #fff;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    height: 600px;
+    overflow: hidden;
+  }
+
+  .login-form-image{
+    height: 100%;
+    overflow: hidden;
+
+          background-size: 100%;
+
+  }
+
+</style>
